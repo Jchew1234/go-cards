eff5bb289656c378746839eecc43578ff6088019
Jacob Chew <jacobjcchew@gmail.com>
2021-11-13 22:37:11 +0000

    Added examples of type conversion to byte slice

--- main.go
+++ main.go
@@ -5,14 +5,18 @@ import "fmt"
 func main() {
 	// cards := deck{"Ace of Diamonds", newCard()}
 	// cards = append(cards, "Six of Spades")
-	cards := newDeck()
 
-	hand, remainingDeck := deal(cards, 5)
-	fmt.Println("deck in hand:")
-	hand.print()
-	fmt.Println("remaining deck:")
-	remainingDeck.print()
-	// cards.print()
+	// cards := newDeck()
+
+	// hand, remainingDeck := deal(cards, 5)
+	// fmt.Println("deck in hand:")
+	// hand.print()
+	// fmt.Println("remaining deck:")
+	// remainingDeck.print()
+	// // cards.print()
+
+	greeting := "Hi there!"
+	fmt.Println([]byte(greeting))
 }
 
 func newCard() string {
